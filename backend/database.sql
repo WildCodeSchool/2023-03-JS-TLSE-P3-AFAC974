ede68d4fbc0ab8072aab3c091862e74bec74a578
Gaspard Caillaud <gaspard.caillaud@gmail.com>
2023-06-14 15:11:29 +0200

    S03US02_server_development added 2 instances of each table

--- backend/database.sql
+++ backend/database.sql
@@ -1,5 +1,6 @@
 -- Table `type`
 DROP TABLE IF EXISTS `type`;
+
 CREATE TABLE IF NOT EXISTS `type` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
@@ -8,6 +9,7 @@ CREATE TABLE IF NOT EXISTS `type` (
 
 -- Table `technique`
 DROP TABLE IF EXISTS `technique`;
+
 CREATE TABLE IF NOT EXISTS `technique` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
@@ -16,6 +18,7 @@ CREATE TABLE IF NOT EXISTS `technique` (
 
 -- Table `art_trend`
 DROP TABLE IF EXISTS `art_trend`;
+
 CREATE TABLE IF NOT EXISTS `art_trend` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
@@ -24,6 +27,7 @@ CREATE TABLE IF NOT EXISTS `art_trend` (
 
 -- Table `artist`
 DROP TABLE IF EXISTS `artist`;
+
 CREATE TABLE IF NOT EXISTS `artist` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `lastname` VARCHAR(45) NOT NULL,
@@ -42,6 +46,7 @@ CREATE TABLE IF NOT EXISTS `artist` (
 
 -- Table `artwork`
 DROP TABLE IF EXISTS `artwork`;
+
 CREATE TABLE IF NOT EXISTS `artwork` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
@@ -69,6 +74,7 @@ CREATE TABLE IF NOT EXISTS `artwork` (
 
 -- Table `entity`
 DROP TABLE IF EXISTS `entity`;
+
 CREATE TABLE IF NOT EXISTS `entity` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
@@ -77,6 +83,7 @@ CREATE TABLE IF NOT EXISTS `entity` (
 
 -- Table `user`
 DROP TABLE IF EXISTS `user`;
+
 CREATE TABLE IF NOT EXISTS `user` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `lastname` VARCHAR(255) NOT NULL,
@@ -93,6 +100,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 -- Table `artwork_favorite`
 DROP TABLE IF EXISTS `artwork_favorite`;
+
 CREATE TABLE IF NOT EXISTS `artwork_favorite` (
   `user_id` INT NOT NULL,
   `artwork_id` INT NOT NULL,
@@ -105,6 +113,7 @@ CREATE TABLE IF NOT EXISTS `artwork_favorite` (
 
 -- Table `artist_technique`
 DROP TABLE IF EXISTS `artist_technique`;
+
 CREATE TABLE IF NOT EXISTS `artist_technique` (
   `artist_id` INT NOT NULL,
   `technique_id` INT NOT NULL,
@@ -117,6 +126,7 @@ CREATE TABLE IF NOT EXISTS `artist_technique` (
 
 -- Table `type_artist`
 DROP TABLE IF EXISTS `type_artist`;
+
 CREATE TABLE IF NOT EXISTS `type_artist` (
   `artist_id` INT NOT NULL,
   `type_id` INT NOT NULL,
@@ -129,6 +139,7 @@ CREATE TABLE IF NOT EXISTS `type_artist` (
 
 -- Table `art_trend_artist`
 DROP TABLE IF EXISTS `art_trend_artist`;
+
 CREATE TABLE IF NOT EXISTS `art_trend_artist` (
   `artist_id` INT NOT NULL,
   `art_trend_id` INT NOT NULL,
@@ -138,3 +149,272 @@ CREATE TABLE IF NOT EXISTS `art_trend_artist` (
   CONSTRAINT `fk_artist_has_art_trend_artist1` FOREIGN KEY (`artist_id`) REFERENCES `artist` (`id`),
   CONSTRAINT `fk_artist_has_art_trend_art_trend1` FOREIGN KEY (`art_trend_id`) REFERENCES `art_trend` (`id`)
 );
+
+-- Table type
+INSERT INTO
+  type (name)
+VALUES
+  ('Abstract'),
+  ('Realism');
+
+INSERT INTO
+  type (name)
+VALUES
+  ('Impressionism'),
+  ('Cubism');
+
+-- Table technique
+INSERT INTO
+  technique (name)
+VALUES
+  ('Oil painting'),
+  ('Watercolor');
+
+INSERT INTO
+  technique (name)
+VALUES
+  ('Sculpture'),
+  ('Photography');
+
+-- Table art_trend
+INSERT INTO
+  art_trend (name)
+VALUES
+  ('Impressionism'),
+  ('Cubism');
+
+INSERT INTO
+  art_trend (name)
+VALUES
+  ('Abstract Expressionism'),
+  ('Pop Art');
+
+-- Table artist
+INSERT INTO
+  artist (
+    lastname,
+    firstname,
+    nickname,
+    description,
+    image_url_small,
+    image_url_medium,
+    image_url_large,
+    website_url,
+    facebook_url,
+    instagram_url,
+    twitter_url
+  )
+VALUES
+  (
+    'Picasso',
+    'Pablo',
+    'Pablo Picasso',
+    'Spanish painter, sculptor, printmaker, ceramicist, and stage designer',
+    'url_small',
+    'url_medium',
+    'url_large',
+    'https://www.pablopicasso.org/',
+    'https://www.facebook.com/pablopicasso',
+    'https://www.instagram.com/pablopicasso',
+    'https://twitter.com/picasso'
+  );
+
+INSERT INTO
+  artist (
+    lastname,
+    firstname,
+    nickname,
+    description,
+    image_url_small,
+    image_url_medium,
+    image_url_large,
+    website_url,
+    facebook_url,
+    instagram_url,
+    twitter_url
+  )
+VALUES
+  (
+    'Monet',
+    'Claude',
+    'Claude Monet',
+    'French painter and a founder of the Impressionist movement',
+    'url_small',
+    'url_medium',
+    'url_large',
+    'https://www.claudemonet.org/',
+    'https://www.facebook.com/claudemonet',
+    'https://www.instagram.com/claudemonet',
+    'https://twitter.com/monet'
+  );
+
+-- Table artwork
+INSERT INTO
+  artwork (
+    name,
+    year,
+    description,
+    image_url_small,
+    image_url_medium,
+    image_url_large,
+    art_trend_id,
+    type_id,
+    technique_id,
+    artist_id,
+    width_cm,
+    height_cm,
+    depth_cm,
+    artwork_location
+  )
+VALUES
+  (
+    'Les Demoiselles d''Avignon',
+    1907,
+    'Oil painting on canvas',
+    'url_small',
+    'url_medium',
+    'url_large',
+    1,
+    1,
+    1,
+    1,
+    244,
+    233,
+    0,
+    'Museum of Modern Art, New York'
+  );
+
+INSERT INTO
+  artwork (
+    name,
+    year,
+    description,
+    image_url_small,
+    image_url_medium,
+    image_url_large,
+    art_trend_id,
+    type_id,
+    technique_id,
+    artist_id,
+    width_cm,
+    height_cm,
+    depth_cm,
+    artwork_location
+  )
+VALUES
+  (
+    'Water Lilies',
+    1914,
+    'Oil painting on canvas',
+    'url_small',
+    'url_medium',
+    'url_large',
+    2,
+    1,
+    2,
+    2,
+    200,
+    180,
+    0,
+    'Musée de l''Orangerie, Paris'
+  );
+
+-- Table entity
+INSERT INTO
+  entity (name)
+VALUES
+  ('Company A'),
+  ('Company B');
+
+INSERT INTO
+  entity (name)
+VALUES
+  ('Organization X'),
+  ('Organization Y');
+
+-- Table user
+INSERT INTO
+  user (
+    lastname,
+    firstname,
+    pseudo,
+    email,
+    password,
+    role,
+    entity_id
+  )
+VALUES
+  (
+    'Doe',
+    'John',
+    'johndoe',
+    'johndoe@example.com',
+    'password',
+    1,
+    NULL
+  );
+
+INSERT INTO
+  user (
+    lastname,
+    firstname,
+    pseudo,
+    email,
+    password,
+    role,
+    entity_id
+  )
+VALUES
+  (
+    'Smith',
+    'Jane',
+    'janesmith',
+    'janesmith@example.com',
+    'password',
+    2,
+    1
+  );
+
+-- Table artwork_favorite
+INSERT INTO
+  artwork_favorite (user_id, artwork_id)
+VALUES
+  (1, 1);
+
+INSERT INTO
+  artwork_favorite (user_id, artwork_id)
+VALUES
+  (2, 2);
+
+-- Table artist_technique
+INSERT INTO
+  artist_technique (artist_id, technique_id)
+VALUES
+  (1, 1);
+
+INSERT INTO
+  artist_technique (artist_id, technique_id)
+VALUES
+  (2, 2);
+
+-- Table type_artist
+INSERT INTO
+  type_artist (artist_id, type_id)
+VALUES
+  (1, 1);
+
+INSERT INTO
+  type_artist (artist_id, type_id)
+VALUES
+  (2, 2);
+
+-- Table art_trend_artist
+INSERT INTO
+  art_trend_artist (artist_id, art_trend_id)
+VALUES
+  (1, 1);
+
+INSERT INTO
+  art_trend_artist (artist_id, art_trend_id)
+VALUES
+  (2, 2);
