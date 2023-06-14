735ea8fc5c4dd308e538cffa414886a0df32741f
JulienC90 <napalm81370@gmail.com>
2023-06-14 10:35:16 +0200

    S03US01_Components_Declarations: set up components SortBy, TypeFilter, UsersDisplay and ValidationModal

---
+++ frontend/src/components/SortBy.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SortBy() {
+  return <div />;
+}
+
+export default SortBy;
---
+++ frontend/src/components/TypeFilter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TypeFilter() {
+  return <div />;
+}
+
+export default TypeFilter;
