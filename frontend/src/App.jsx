2cd8b485304c650ab1defea532dd0bf458b11acb
Gaspard Caillaud <gaspard.caillaud@gmail.com>
2023-06-19 15:35:09 +0200

    NavBar initialization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <NavBar />
         <Home />
         <p>coucou</p>
       </div>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Turn as Hamburger } from "hamburger-react";
 
 function NavBar() {
-  return <div />;
+  return (
+    <div className="navbar-header">
+      <img className="logo" src="/src/assets/navbar_logo.png" alt="logo" />
+      <div className="navbar-links">
+        <img className="flag-logo" src="/src/assets/flag_logo.png" alt="flag" />
+        <img
+          className="login-logo"
+          src="/src/assets/login_logo.png"
+          alt="login"
+        />
+        <div className="Hamburger">
+          <Hamburger />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
