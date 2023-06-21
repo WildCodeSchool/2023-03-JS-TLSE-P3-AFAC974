79bebe3677e7dcd67000c7f37c3f046ebf872387
Julien CAMPILLO <126490246+JulienC90@users.noreply.github.com>
2023-06-21 10:02:39 +0200

    Update ArtworkManager.js
    Body destructuration on 'createArtwork' function and added dynamic update function on 'updateArtwork'

--- backend/src/models/ArtworkManager.js
+++ backend/src/models/ArtworkManager.js
@@ -7,23 +7,40 @@ class ArtworkManager extends AbstractManager {
   }
 
   createArtwork(artwork) {
+    const {
+        name,
+        year,
+        description,
+        image_url_small,
+        image_url_medium,
+        image_url_large,
+        art_trend_id,
+        type_id,
+        technique_id,
+        artist_id,
+        width_cm,
+        height_cm,
+        depth_cm,
+        artwork_location,
+    } = body;
+    
     return this.database.query(
       `insert into ${this.table} (name, year, description, image_url_small, image_url_medium, image_url_large, art_trend_id, type_id, technique_id, artist_id, width_cm, height_cm, depth_cm, artwork_location) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)`,
       [
-        artwork.name,
-        artwork.year,
-        artwork.description,
-        artwork.image_url_small,
-        artwork.image_url_medium,
-        artwork.image_url_large,
-        artwork.art_trend_id,
-        artwork.type_id,
-        artwork.technique_id,
-        artwork.artist_id,
-        artwork.width_cm,
-        artwork.height_cm,
-        artwork.depth_cm,
-        artwork.artwork_location,
+        name,
+        year,
+        description,
+        image_url_small,
+        image_url_medium,
+        image_url_large,
+        art_trend_id,
+        type_id,
+        technique_id,
+        artist_id,
+        width_cm,
+        height_cm,
+        depth_cm,
+        artwork_location,
       ]
     );
   }
@@ -45,71 +62,23 @@ class ArtworkManager extends AbstractManager {
       depth_cm,
       artwork_location,
     } = body;
+    
+    const { id } = req.params;
+    
+    let keys = Object.keys(req.body);
+    const values = Object.values(req.body);
+    const valueQuery = keys.map((key) => `${key} = ?`).join(', ');
 
-    let query = `update ${this.table} set `;
-    const values = [];
-
-    if (name) {
-      query += `name = ?, `;
-      values.push(name);
+    db.query(`update ${this.table} set ${valueQuery}`, [...values, id]).then(
+    ([result]) => {
+      if (result.affectedRows > 0) {
+        res.status(204).json(result);
+      } else {
+        res.status(404).send('Cannot update artwork');
+      }
     }
-    if (year) {
-      query += `year = ?, `;
-      values.push(year);
-    }
-    if (description) {
-      query += `description = ?, `;
-      values.push(description);
-    }
-    if (image_url_small) {
-      query += `image_url_small = ?, `;
-      values.push(image_url_small);
-    }
-    if (image_url_medium) {
-      query += `image_url_medium = ?, `;
-      values.push(image_url_medium);
-    }
-    if (image_url_large) {
-      query += `image_url_large = ?, `;
-      values.push(image_url_large);
-    }
-    if (art_trend_id) {
-      query += `art_trend_id = ?, `;
-      values.push(art_trend_id);
-    }
-    if (type_id) {
-      query += `type_id = ?, `;
-      values.push(type_id);
-    }
-    if (technique_id) {
-      query += `technique_id = ?, `;
-      values.push(technique_id);
-    }
-    if (artist_id) {
-      query += `artist_id = ?, `;
-      values.push(artist_id);
-    }
-    if (width_cm) {
-      query += `width_cm = ?, `;
-      values.push(width_cm);
-    }
-    if (height_cm) {
-      query += `height_cm = ?, `;
-      values.push(height_cm);
-    }
-    if (depth_cm) {
-      query += `depth_cm = ?, `;
-      values.push(depth_cm);
-    }
-    if (artwork_location) {
-      query += `artwork_location = ?, `;
-      values.push(artwork_location);
-    }
-    query = query.slice(0, -2);
-    query += ` where id = ?`;
-    values.push(id);
-    return this.database.query(query, values);
+  );
   }
 }
-
+   
 module.exports = ArtworkManager;
