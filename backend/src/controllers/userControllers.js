db48f513d9a3529ceed433152d3c78a31874c19c
JulienC90 <napalm81370@gmail.com>
2023-06-21 16:51:48 +0200

    S04US09_User-route: Added UserManager, userControllers, auth, modified router, index, database, and fixed ArtworkManager on update query, artworkControllers, fixed argon2 & jwt mentions in package.json on frontend and backend folders

---
+++ backend/src/controllers/userControllers.js
@@ -0,0 +1,105 @@
+const models = require("../models");
+
+const browse = (req, res) => {
+  models.user
+    .findAll()
+    .then(([rows]) => {
+      res.send(rows);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const read = (req, res) => {
+  const { id } = req.params;
+  models.user
+    .find(id)
+    .then(([rows]) => {
+      if (rows.length === 0) {
+        res.sendStatus(404);
+      } else {
+        res.send(rows).status(200);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const add = (req, res) => {
+  const data = req.body;
+  models.user
+    .insert(data)
+    .then(([result]) => {
+      res.location(`/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const edit = (req, res) => {
+  const { id } = req.params;
+  models.user
+    .update(id, req.body)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const login = (req, res, next) => {
+  const email = req.body;
+  models.user
+    .login(email)
+    .then(([users]) => {
+      if (users[0] != null) {
+        req.user = { ...users[0] };
+
+        next();
+      } else {
+        res.sendStatus(401);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const destroy = (req, res) => {
+  const { id } = req.params;
+  models.user
+    .delete(id)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  browse,
+  read,
+  add,
+  edit,
+  login,
+  destroy,
+};
--- backend/src/router.js
+++ backend/src/router.js
@@ -2,6 +2,17 @@ const express = require("express");
 
 const router = express.Router();
 
+// authentication import
+
+const {
+  verifyEmail,
+  hashPassword,
+  verifyPassword,
+  verifyToken,
+} = require("./auth");
+
+// item routes
+
 const itemControllers = require("./controllers/itemControllers");
 
 router.get("/items", itemControllers.browse);
@@ -15,13 +26,24 @@ router.delete("/items/:id", itemControllers.destroy);
 const artworkControllers = require("./controllers/artworkControllers");
 
 router.get("/artworks", artworkControllers.browse);
-router.post("/artworks", artworkControllers.create);
-router.put("/artworks/:id", artworkControllers.edit);
+router.post("/artworks", verifyToken, artworkControllers.create);
+router.put("/artworks/:id", verifyToken, artworkControllers.edit);
 
 // artist routes
 
 const artistControllers = require("./controllers/artistControllers");
 
 router.get("/artists", artistControllers.browse);
+
+// user routes
+
+const userControllers = require("./controllers/userControllers");
+
+router.get("/users", userControllers.browse);
+router.get("/users/:id", userControllers.read);
+router.post("/register", verifyEmail, hashPassword, userControllers.add);
+router.post("/login", userControllers.login, verifyPassword);
+router.put("/users/:id", userControllers.edit);
+router.delete("/users/:id", userControllers.destroy);
 
 module.exports = router;
