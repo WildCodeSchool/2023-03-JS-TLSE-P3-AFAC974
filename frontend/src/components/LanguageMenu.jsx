99088799e0d9baab856e880352ccf4fcae66f5b0
Gaspard Caillaud <gaspard.caillaud@gmail.com>
2023-06-21 10:30:08 +0200

    S04US06_NavBar created modals for language and login. Modals are opened from NavBar, no style

--- frontend/src/components/LanguageMenu.jsx
+++ frontend/src/components/LanguageMenu.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import ReactModal from "react-modal";
+import PropTypes from "prop-types";
 
-function LanguageMenu() {
-  return <div />;
+function LanguageMenu({ languageModalOpened, setLanguageModalOpened }) {
+  return (
+    <ReactModal
+      isOpen={languageModalOpened}
+      onRequestClose={() => setLanguageModalOpened(false)}
+      style={{
+        overlay: {
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+        },
+        content: {
+          backgroundColor: "#fff",
+          color: "#000",
+          overflow: "hidden",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "40%",
+          width: "40%",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        },
+      }}
+    >
+      <div>
+        <h1>Choisissez une langue</h1>
+        <div>
+          <img src="" alt="" />
+          <p>Français</p>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <p>English</p>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <p>Créole</p>
+        </div>
+      </div>
+    </ReactModal>
+  );
 }
+
+LanguageMenu.propTypes = {
+  languageModalOpened: PropTypes.bool.isRequired,
+  setLanguageModalOpened: PropTypes.func.isRequired,
+};
 
 export default LanguageMenu;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -36,99 +36,107 @@ function NavBar() {
   };
 
   return (
-    <div className="navbar-header flex justify-between items-center bg-[#257492] h-[52px] lg:h-[60px] px-3">
-      <img
-        className="logo h-[35px] w-auto sm:h-[55px] "
-        src="/src/assets/navbar_logo.png"
-        alt="logo"
+    <>
+      <div className="navbar-header flex justify-between items-center bg-[#257492] h-[52px] lg:h-[60px] px-3">
+        <img
+          className="logo h-[35px] w-auto sm:h-[55px] "
+          src="/src/assets/navbar_logo.png"
+          alt="logo"
+        />
+        <div className="desktopLinks hidden sm:flex navbar-links items-center gap-[120px] text-white ">
+          <div
+            className={`flex items-center gap-2 w-[28px] ${
+              homeHovered ? "hovered" : ""
+            }`}
+            onMouseEnter={handleHomeHover}
+            onMouseLeave={handleHomeLeave}
+          >
+            <img
+              src={
+                homeHovered
+                  ? "/src/assets/hexagon_red_bg.png"
+                  : "/src/assets/hexagon_blue_bg.png"
+              }
+              alt="hexagon"
+              className="h-[28px] w-[26.32px]"
+            />
+            <Link to="/">HOME</Link>
+          </div>
+          <div
+            className={`flex items-center gap-2 w-[28px] ${
+              galleryHovered ? "hovered" : ""
+            }`}
+            onMouseEnter={handleGalleryHover}
+            onMouseLeave={handleGalleryLeave}
+          >
+            <img
+              src={
+                galleryHovered
+                  ? "/src/assets/hexagon_red_bg.png"
+                  : "/src/assets/hexagon_blue_bg.png"
+              }
+              alt="hexagon"
+              className="h-[28px] w-[26.32px]"
+              id="galleryPicture"
+            />
+            <Link to="/gallery">GALERIE</Link>
+          </div>
+          <div
+            className={`flex items-center gap-2 w-[28px] whitespace-nowrap ${
+              aboutHovered ? "hovered" : ""
+            }`}
+            onMouseEnter={handleAboutHover}
+            onMouseLeave={handleAboutLeave}
+          >
+            <img
+              src={
+                aboutHovered
+                  ? "/src/assets/hexagon_red_bg.png"
+                  : "/src/assets/hexagon_blue_bg.png"
+              }
+              alt="hexagon"
+              className="h-[28px] w-[26.32px] "
+              id="aboutPicture"
+            />
+            <Link to="/about">A PROPOS</Link>
+          </div>
+        </div>
+        <div className="navbar-links flex items-center gap-1.7 sm:gap-[10px] ">
+          <button onClick={() => setLanguageModalOpened(true)} type="button">
+            <img
+              className="flag-logo px-1.5"
+              src="/src/assets/flag_logo.png"
+              alt="flag"
+            />
+          </button>
+          <button
+            onClick={() => setLoginModalOpened(true)}
+            type="button"
+            className="flex items-center gap-2 text-sm font-semibold"
+          >
+            <p className="hidden sm:block text-white ">COMPTE</p>
+            <img
+              className="login-logo h-[24px] px-1.5"
+              src="/src/assets/login_logo.png"
+              alt="login"
+            />
+          </button>
+          <div className="Hamburger lg:hidden">
+            <Hamburger color="#fff" size={24} />
+          </div>
+        </div>
+      </div>
+
+      <LanguageMenu
+        languageModalOpened={languageModalOpened}
+        setLanguageModalOpened={setLanguageModalOpened}
       />
-      <div className="desktopLinks hidden sm:flex navbar-links items-center gap-[120px] text-white ">
-        <div
-          className={`flex items-center gap-2 w-[28px] ${
-            homeHovered ? "hovered" : ""
-          }`}
-          onMouseEnter={handleHomeHover}
-          onMouseLeave={handleHomeLeave}
-        >
-          <img
-            src={
-              homeHovered
-                ? "/src/assets/hexagon_red_bg.png"
-                : "/src/assets/hexagon_blue_bg.png"
-            }
-            alt="hexagon"
-            className="h-[28px] w-[26.32px]"
-          />
-          <Link to="/">HOME</Link>
-        </div>
-        <div
-          className={`flex items-center gap-2 w-[28px] ${
-            galleryHovered ? "hovered" : ""
-          }`}
-          onMouseEnter={handleGalleryHover}
-          onMouseLeave={handleGalleryLeave}
-        >
-          <img
-            src={
-              galleryHovered
-                ? "/src/assets/hexagon_red_bg.png"
-                : "/src/assets/hexagon_blue_bg.png"
-            }
-            alt="hexagon"
-            className="h-[28px] w-[26.32px]"
-            id="galleryPicture"
-          />
-          <Link to="/gallery">GALERIE</Link>
-        </div>
-        <div
-          className={`flex items-center gap-2 w-[28px] whitespace-nowrap ${
-            aboutHovered ? "hovered" : ""
-          }`}
-          onMouseEnter={handleAboutHover}
-          onMouseLeave={handleAboutLeave}
-        >
-          <img
-            src={
-              aboutHovered
-                ? "/src/assets/hexagon_red_bg.png"
-                : "/src/assets/hexagon_blue_bg.png"
-            }
-            alt="hexagon"
-            className="h-[28px] w-[26.32px] "
-            id="aboutPicture"
-          />
-          <Link to="/about">A PROPOS</Link>
-        </div>
-      </div>
-      <div className="navbar-links flex items-center gap-1.7 sm:gap-[10px] ">
-        <button onClick={() => setLanguageModalOpened(true)} type="button">
-          <img
-            className="flag-logo px-1.5"
-            src="/src/assets/flag_logo.png"
-            alt="flag"
-          />
-        </button>
-        <button
-          onClick={() => setLoginModalOpened(true)}
-          type="button"
-          className="flex items-center gap-2 text-sm font-semibold"
-        >
-          <p className="hidden sm:block text-white ">COMPTE</p>
-          <img
-            className="login-logo h-[24px] px-1.5"
-            src="/src/assets/login_logo.png"
-            alt="login"
-          />
-        </button>
-        <div className="Hamburger lg:hidden">
-          <Hamburger color="#fff" size={24} />
-        </div>
-      </div>
-      {languageModalOpened && (
-        <LanguageMenu setLanguageModalOpened={setLanguageModalOpened} />
-      )}
-      {loginModalOpened && <Login setLoginModalOpened={setLoginModalOpened} />}
-    </div>
+
+      <Login
+        loginModalOpened={loginModalOpened}
+        setLoginModalOpened={setLoginModalOpened}
+      />
+    </>
   );
 }
 
