9aecac37782b31cf895b7d5913a3556e258a1765
Gaspard Caillaud <gaspard.caillaud@gmail.com>
2023-07-03 10:40:51 +0200

    S05US18_Artist_manager_controller created routes, controllers and managers for artist

--- backend/src/models/ArtistManager.js
+++ backend/src/models/ArtistManager.js
@@ -1,8 +1,56 @@
+/* eslint-disable camelcase */
 const AbstractManager = require("./AbstractManager");
 
 class ArtistManager extends AbstractManager {
   constructor() {
     super({ table: "artist" });
+  }
+
+  createArtist(artist) {
+    const {
+      lastname,
+      firstname,
+      nickname,
+      description,
+      image_url_small,
+      image_url_medium,
+      image_url_large,
+      website_url,
+      facebook_url,
+      instagram_url,
+      twitter_url,
+    } = artist;
+
+    return this.database.query(
+      `INSERT INTO ${this.table} (lastname, firstname, nickname, description, image_url_small, image_url_medium, image_url_large, website_url, facebook_url, instagram_url, twitter_url) VALUES (?,?,?,?,?,?,?,?,?,?,?)`,
+      [
+        lastname,
+        firstname,
+        nickname,
+        description,
+        image_url_small,
+        image_url_medium,
+        image_url_large,
+        website_url,
+        facebook_url,
+        instagram_url,
+        twitter_url,
+      ]
+    );
+  }
+
+  updateArtist(id, body) {
+    const keys = Object.keys(body);
+    const values = Object.values(body);
+    const valueQuery = keys.map((key) => `${key} = ?`).join(", ");
+    return this.database.query(
+      `update ${this.table} set ${valueQuery} where id = ?`,
+      [...values, id]
+    );
+  }
+
+  deleteArtist(id) {
+    return this.database.query(`delete from ${this.table} where id = ?`, [id]);
   }
 }
 
