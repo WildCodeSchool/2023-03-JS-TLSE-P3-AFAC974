265ebdcd361f56c142e783fef3895a48e1fae169
Gaspard Caillaud <gaspard.caillaud@gmail.com>
2023-07-10 16:58:54 +0200

    S06US26_UserPage search bar et filter opérationnels

--- frontend/src/components/UsersDisplay.jsx
+++ frontend/src/components/UsersDisplay.jsx
@@ -8,31 +8,69 @@ import userSample from "../assets/user_sample.png";
 function UsersDisplay() {
   const [data, setData] = useState([]);
   const [deletedUserId, setDeletedUserId] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filter, setFilter] = useState("");
+  const [filteredAndSortedData, setFilteredAndSortedData] = useState([]);
 
-  const handleUsersSorting = (someData) => {
-    return someData.sort((a, b) => {
-      if (a.pseudo < b.pseudo) {
-        return -1;
-      }
-      if (a.pseudo > b.pseudo) {
-        return 1;
-      }
-      return 0;
-    });
+  const filterAndSortData = () => {
+    let sortedData = [...data];
+    if (searchTerm) {
+      sortedData = sortedData.filter((item) => {
+        if (item.lastname && typeof item.lastname === "string") {
+          return item.lastname.toLowerCase().includes(searchTerm.toLowerCase());
+        }
+        return false;
+      });
+    }
+
+    if (filter === "asc") {
+      sortedData.sort((a, b) => {
+        if (a.pseudo && b.pseudo) {
+          return a.pseudo.localeCompare(b.pseudo);
+        }
+        return 0;
+      });
+    } else if (filter === "desc") {
+      sortedData.sort((a, b) => {
+        if (a.pseudo && b.pseudo) {
+          return b.pseudo.localeCompare(a.pseudo);
+        }
+        return 0;
+      });
+    }
+    return sortedData;
+  };
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleChange = (e) => {
+    setFilter(e.target.value);
   };
 
   useEffect(() => {
-    axios.get(`${import.meta.env.VITE_BACKEND_URL}/users`).then((res) => {
-      const data2 = res.data;
-      setData(data2);
-    });
+    const filteredAndSorted = filterAndSortData();
+    setFilteredAndSortedData(filteredAndSorted);
+  }, [data, searchTerm, filter]);
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}/users`)
+      .then((res) => {
+        const data2 = res.data;
+        setData(data2);
+      })
+      .catch((error) => {
+        console.error("Error fetching user data:", error);
+      });
   }, [deletedUserId]);
 
   return (
     <div className="absolute mt-[60px] w-full flex flex-col items-center">
       <div className="flex justify-center sm:justify-between items-center w-[100%]">
         <div className="flex flex-col  md:flex-row justify-between items-center gap-[16px] md:gap-[50px] p-[20px] mx-[50px]">
-          <div className="imageCircleContainer w-[110px] h-[110px] border border-0.5 border-gray-500 border-solid  rounded-full overflow-hidden ">
+          <div className="imageCircleContainer w-[110px] h-[110px] border border-0.5 border-gray-500 border-solid  rounded-full overflow-hidden">
             <img
               src={userSample}
               alt="profile sample"
@@ -42,25 +80,22 @@ function UsersDisplay() {
           <p>Gestion des utilisateurs</p>
         </div>
         <div>
-          <p className="hidden md:block mx-[70px] ">Bouton retour</p>
+          <p className="hidden md:block mx-[70px]">Bouton retour</p>
         </div>
       </div>
-      <div className="flex flex-col-reverse sm:flex-row sm:justify-between sm:w-[100%] items-center sm:px-[100px] ">
-        <SortBy
-          handleChange={() => {
-            handleUsersSorting(data);
-          }}
+      <div className="flex flex-col-reverse sm:flex-row sm:justify-between sm:w-[100%] items-center sm:px-[100px]">
+        <SortBy handleChange={handleChange} />
+        <SearchBar
+          searchTerm={searchTerm}
+          handleInputChange={handleInputChange}
         />
-        <SearchBar searchTerm="hey" handleInputChange="blabla" />
         <p className="text-transparent sm:w-[15%]">|</p>
       </div>
-      {data.map((user) => {
-        return (
-          <div key={user.id} className="w-[100%] sm:px-[100px]">
-            <UserCard user={user} setDeletedUserId={setDeletedUserId} />
-          </div>
-        );
-      })}
+      {filteredAndSortedData.map((user) => (
+        <div key={user.id} className="w-[100%] sm:px-[100px]">
+          <UserCard user={user} setDeletedUserId={setDeletedUserId} />
+        </div>
+      ))}
     </div>
   );
 }
