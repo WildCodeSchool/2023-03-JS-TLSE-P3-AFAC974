9f6a80eac13b4c72f512bbdf461753bb299d31b8
Gaspard Caillaud <gaspard.caillaud@gmail.com>
2023-07-18 12:17:19 +0200

    S07US36_UserFavorite Favorite display is functional for artworks

--- frontend/src/pages/UserFavorite.jsx
+++ frontend/src/pages/UserFavorite.jsx
@@ -5,8 +5,8 @@ import AuthContext from "../context/AuthContext";
 import SortBy from "../components/SortBy";
 
 function UserFavorite() {
-  const [favData, setFavData] = useState([]);
-  const [setFavArts] = useState([]);
+  const [artworksToMap, setArtworksToMap] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
   const { userId } = React.useContext(AuthContext);
 
   useEffect(() => {
@@ -15,48 +15,54 @@ function UserFavorite() {
         `${import.meta.env.VITE_BACKEND_URL}/user/${userId}/artworks/favorites`
       )
       .then((response) => {
-        console.info(response.data);
-        setFavData(response.data);
+        const favData = response.data;
+        const fetchArtworksPromises = favData.map((fav) =>
+          axios.get(
+            `${import.meta.env.VITE_BACKEND_URL}/artworks/${fav.artwork_id}`
+          )
+        );
+        Promise.all(fetchArtworksPromises)
+          .then((artworksResponses) => {
+            const artworksData = artworksResponses.map((res) => res.data[0]);
+            setArtworksToMap(artworksData);
+            setIsLoaded(true);
+          })
+          .catch((error) => {
+            console.error(error);
+            setIsLoaded(true);
+          });
       })
       .catch((error) => {
         console.error(error);
-      });
-  }, []);
-
-  useEffect(() => {
-    axios
-      .get(`${import.meta.env.VITE_BACKEND_URL}/artworks`)
-      .then((response) => {
-        setFavArts(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
+        setIsLoaded(true);
       });
   }, []);
 
   return (
-    <div className="flex flex-col justify-center">
-      <div className="px-[20px] mt-[150px] flex flex-row justify-between">
-        <h1 className="font-semibold text-4xl text-slate-300">
-          Mes Favoris ({favData.length})
-        </h1>
-        <SortBy />
+    isLoaded && (
+      <div className="flex flex-col justify-center">
+        <div className="px-[20px] mt-[150px] flex flex-row justify-between">
+          <h1 className="font-semibold text-4xl text-slate-300">
+            Mes Favoris ({artworksToMap.length})
+          </h1>
+          <SortBy />
+        </div>
+        {artworksToMap.map((item) => {
+          return (
+            <div
+              key={item.id}
+              className="flex flex-col justify-center items-center"
+            >
+              <Link to={`/artwork/${item.artworkId}`}>
+                <div className="flex flex-col justify-center items-center">
+                  <img src={item.image_url_medium} alt="artwork" />
+                </div>
+              </Link>
+            </div>
+          );
+        })}
       </div>
-      {favData.map((item) => {
-        return (
-          <div
-            key={item.id}
-            className="flex flex-col justify-center items-center"
-          >
-            <Link to={`/artwork/${item.artworkId}`}>
-              <div className="flex flex-col justify-center items-center">
-                <img src={item.image_url_medium} alt="artwork" />
-              </div>
-            </Link>
-          </div>
-        );
-      })}
-    </div>
+    )
   );
 }
 
