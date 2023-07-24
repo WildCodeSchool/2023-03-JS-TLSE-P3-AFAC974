e95a8e8edd5607ef5fd85370f276d947dc9a474c
Gaspard Caillaud <gaspard.caillaud@gmail.com>
2023-07-24 12:13:15 +0200

    S08US40_Main_styling improvement on homepage : sizing, responsive, full-page minus navbar

--- frontend/src/components/GalleryButton.jsx
+++ frontend/src/components/GalleryButton.jsx
@@ -4,7 +4,7 @@ import hexagonBlueBg from "../assets/hexagon_blue_bg.png";
 function GalleryButton() {
   return (
     <button
-      className=" w-[190px] h-[45px] flex flex-row justify-center items-center gap-4 bg-[#7F253E] text-[#e2e3e4] px-[10px] py-[2px] rounded-[8px] text-[18px] shadow-[5px_5px_15px_#222] md:h-[60px] md:w-[280px] md:text-[21px]"
+      className=" w-[170px]  h-[45px] flex flex-row justify-center items-center gap-4 bg-[#7F253E] text-[#e2e3e4] px-[10px] py-[2px] rounded-[8px] text-[16px] shadow-[5px_5px_15px_#222] md:h-[60px] md:w-[280px] md:text-[21px]"
       type="button"
     >
       <img className="hidden md:block" src={hexagonBlueBg} alt="logo bleu" />
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,17 +3,19 @@ import GalleryButton from "../components/GalleryButton";
 
 export default function Home() {
   return (
-    <div className="h-screen w-[100%] relative bg-cover z-0 bg-no-repeat bg-center opacity-95 custom-bg">
-      <div className="flex flex-col h-[100%] justify-center items-center gap-28 md:gap-48">
-        <h1 className="font-bold mt-14 text-white text-4xl drop-shadow-[5px_5px_5px_#222] md:text-[53px] md:mt-10">
-          Bienvenue à l'AFAC
-        </h1>
-        <div className="flex flex-col justify-around h-[20%] font-semibold text-white text-3xl drop-shadow-[5px_5px_3px_#222] md:text-[42px] lg:flex lg:flex-row lg:justify-between lg:w-[40%] lg:items-center lg:text-[42px] lg:h-[5%]">
-          <h3>Éducation</h3>
-          <h3>Culture</h3>
-          <h3>Patrimoine</h3>
+    <div className=" mt-[52px] lg:mt-[60px] bg-cover z-0 bg-no-repeat bg-center opacity-95 custom-bg overflow-y-hidden">
+      <div className="h-full flex flex-col  justify-between items-center h-screen-minus-navbar  py-[20%] md:py-[32px]">
+        <div className="flex flex-col justify-between h-[60%] md:h-[45%] ">
+          <h1 className="font-bold text-white text-3xl md:text-4xl drop-shadow-[4px_4px_4px_#222] md:mt-[32px]">
+            Bienvenue à l'AFAC
+          </h1>
+          <div className="flex flex-col md:flex-row gap-5 md:gap-20 text-white text-xl md:text-3xl drop-shadow-[3px_3px_3px_#222]">
+            <h3>Éducation</h3>
+            <h3>Culture</h3>
+            <h3>Patrimoine</h3>
+          </div>
         </div>
-        <div className="flex justify-center h-[40px] mt-4">
+        <div className="">
           <Link to="/gallery">
             <GalleryButton />
           </Link>
