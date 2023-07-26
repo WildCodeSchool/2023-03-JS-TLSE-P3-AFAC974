79e3895a3593ef608c109a846eb4a71d1905c51c
Marie-Hermine Schneider <32041949+aimach@users.noreply.github.com>
2023-07-26 11:15:56 +0200

    Update database.sql

--- backend/database.sql
+++ backend/database.sql
@@ -182,7 +182,7 @@ VALUES
     'Hyppolite Charles Napoléon Mortier de Trévise',
     'Hippolyte Charles Napoléon Mortier de Trévise, 3e duc de Trévise, naquit le 4 mai 1835 à Paris, France. Issu dune famille noble, il fut destiné à jouer un rôle important dans la société. Il embrassa une carrière diplomatique en tant que secrétaire d''ambassade sous Napoléon III et accéda aux titres de pair de France et de sénateur. En 1860, il épousa Marie Angèle Emma Le Coat de Kerveguen, mais le couple n''eut pas d''enfants. Outre ses engagements politiques, Hippolyte se révéla être un talentueux artiste, notamment dans le dessin et l''aquarelle, dont certaines œuvres sont conservées au Musée Léon-Dierx. Impliqué dans les affaires familiales, il géra un empire foncier hérité de son beau-père, comprenant des usines sucrières à La Réunion. Après la chute du Second Empire, il continua d''influencer les sphères économiques. Hippolyte décéda le 13 février 1892, laissant derrière lui un héritage artistique et un rôle marqué au sein de l''aristocratie française du XIXe siècle.',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690185021/artist-afac/m8rx9vfqkgxfiktjoc8p.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690293385/artist-afac/xmxkqubx0nqupfx9n69v.jpg',
     'url_large',
     'https://fr.wikipedia.org/wiki/Hippolyte_Mortier_de_Tr%C3%A9vise',
     'url_facebook',
@@ -214,7 +214,7 @@ VALUES
     '1866',
     'Les chevaux sont rares sur les établissements: ils font l''objet de soins attentifs, et ne sont montés que par les propriétaires des Etablissements, et les contremaîtres. Selle et cuirs peuvent être fabriqués sur place: il y eut un atelier sur l''Etablissement du Tampon.',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690186385/artwork-afac/rkuswdhoyupyapfthdl0.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690291996/artwork-afac/jwkhhtmvfjpc8kfs34pc.jpg',
     'url_large',
     1,
     1,
@@ -249,7 +249,7 @@ VALUES
     '1861',
     '4 mulets tirent une charrette apportant des cannes frâichement coupées à l''usine. Les mulets sont nombreux dans l''île à l''époque de l''industrialisation sucrière. Importés du Poitou, ce sont des bêtes robustes, qui coûtent moins chers que des boeufs ou des chevaux, pour lesquelles on construit des écuries. Elles ont des noms: on sait que dans l''Etablissement des Casernes, Tec Tec, Langoutil, et Malheur sont des noms de mules.',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690187341/artwork-afac/mmynylo6wgsh6oa9mpam.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690291996/artwork-afac/oulleipq4bxvhal2pxfx.jpg',
     'url_large',
     1,
     1,
@@ -284,7 +284,7 @@ VALUES
     '1861',
     'En réalité, la caille fut introduite d''Asie, Inde ou chine, vers 1850. C''est la femelle qui est colorée ainsi de rouge au bas des ailes. A l''époque de Mortier de Trévise, c''est donc une curiosité, un peu en disparition, à cause de l''extension des champs cultivés en cannes à sucre.',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690187776/artwork-afac/uoiar7eawdrivqfqes0o.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690292751/artwork-afac/fj3kxdsb1ge7a8dkgkrz.jpg',
     'url_large',
     1,
     1,
@@ -319,7 +319,7 @@ VALUES
     '1865',
     'Titre complet: "Sortie du Bras de Jean Payet en allant vers le Tampon". Le tilbury à quatre roues est tiré par quatre mules (importées du Poitou). La route, encore reconnaissable aujourd''hui, reliait les chmps de canne situés entre la ravin e Jean Payet (ancienne ravine du Tampon), et la ravine des Cafres. au sommet de ces champs, une scierie fournissait le bois et les planches pour les Etablissements K/Véguen',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690190308/artwork-afac/qtkszwzbdbx4mvmhgfgr.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690292969/artwork-afac/ohuhi0ukanbgjfw8li8k.jpg',
     'url_large',
     1,
     1,
@@ -354,7 +354,7 @@ VALUES
     '1866',
     'Titre complet: "Le bassin rouge au Tampon, la ravine descend". La cascade alimente un bassin à proximité d''un affluent de la rivière d''Abord.',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690190657/artwork-afac/duv1y1has2iuhvzft4cy.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690291995/artwork-afac/n29zlqoyki6r8mksfuat.jpg',
     'url_large',
     1,
     1,
@@ -389,7 +389,7 @@ VALUES
     '1861',
     'Titre complet: "Excursion au volcan de Bourbon". Mortier de Trévise et sa belle-famille sont ne excursion au volcan. Il n''y avait pas de route, alors: il faut donc dormir en chemin dans cette caverne autrefois connue des noirs marrons, autrement dit fugitifs -avant  l''abolition de l''escalvage de 1848.',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690191124/artwork-afac/lqgyzii6oodbhidugfyt.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690292920/artwork-afac/zfemsdixh8mxxexnfosw.jpg',
     'url_large',
     1,
     1,
@@ -424,7 +424,7 @@ VALUES
     '1861',
     'Titre complet: "Le volcan de Bourbon vu du Pas de Bellecombre". Cela ne fait guère longtemps que le passage par le Pas de Bellecombe a été trouvé. Le lieu porte le nom du gouverneur présent au moment de la découverte du passage, mais c''est un esclave, Jacob, qui l''a découvert, en réalité. Bellecombe avait commandité l''expédition.',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690191463/artwork-afac/xqajbrmyloaultudyq7w.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690291996/artwork-afac/ctxivohu2hbgyshlzmm1.jpg',
     'url_large',
     1,
     1,
@@ -459,7 +459,7 @@ VALUES
     '1865',
     'Travailleur engagé depuis l''Inde à l''Etablissement des Casernes, il tient une lance, peut-être a-t-il une fonction de gardien? Sur sa fiche d''engagement, il était recensé comme tailleur.',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690193260/artwork-afac/vxise3al67sjtu2ht6wm.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690291995/artwork-afac/ceym0pv473ufrxyjfip3.jpg',
     'url_large',
     1,
     1,
@@ -494,7 +494,7 @@ VALUES
     '1865',
     'Complète la précédente aquarelle. On devine la jeunesse de Boutchiana, engagé à l''adolescence. Arrivé à bord de Yanaon, en Inde, à bord du navire de la famille Kerveguen, Le Canova, on le dit âgé de 17 ans.',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690193061/artwork-afac/ykcj4lecgbufxyyfr01n.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690291995/artwork-afac/gnbu8mhknubneah2evfk.jpg',
     'url_large',
     1,
     1,
@@ -529,7 +529,7 @@ VALUES
     '1861',
     'C''est une engagée, ou alors une affranchie. Elle porte la robe de toile bleue, dont la fourniture est obligatoire par l''employeur, selon les termes du contrat d''engagement. La pratique ne change guère de ce qui était déjà prévu avant 1848 pour les esclaves, par le "Code noir" de 1723.',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690193413/artwork-afac/kiyatmpq9nkfubedmpin.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690291996/artwork-afac/m7o7odinqoadcbhc5ee9.jpg',
     'url_large',
     1,
     1,
@@ -564,7 +564,7 @@ VALUES
     '1861',
     '"Cafre" veut dire que Jamali n''est pas né sur l''Habitation, mais qu''il a vraisemblablement été recruté comme engagé. Il est armé d''une lance, et surveille l''orée des champs, ou les abords du camp des travailleurs.',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690193712/artwork-afac/gxedvk3j83lgjai1qwop.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690292824/artwork-afac/my5clkzivxkssrjxnfct.jpg',
     'url_large',
     1,
     1,
@@ -599,7 +599,7 @@ VALUES
     '1861',
     'Le titre de citoyen est une fierté pour les affranchis de 1848 qui travaillent sur la propriété ou dans les Etablissements K/Véguen. La pluie est rare à Saint-Pierre, beaucoup plus fréquente au Tampon (pluies orographiques pendant la saison chaude, celle de la coupe des cannes). Ici, le créole engagé dispose d''une maigre rémunération, juste suffisante pour sa nourriture et de menus frais à la "boutique". Depuis 1859, le salaire est en outre versé en kreutzers ( démonétisés, au cours forcé de 1 franc. A l''arrière-plan, sur la droite, la silhouette d''une cheminée d''usine, peut-être celle de Bel-Air, au Tampon.',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690193877/artwork-afac/blgq0sfrgpgmoucpdby6.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690291994/artwork-afac/eeb846xrmgvvro9uwsxx.jpg',
     'url_large',
     1,
     1,
@@ -634,7 +634,7 @@ VALUES
     '1865',
     'La vieille dame est assise sur une natte, vêtue de la traditionnelle robe de toile bleue fournie par l''employeur. Son foulard noué sur la tête est taillé dans la même toile.',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690194094/artwork-afac/awv20udptijd8bqamsjq.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690291994/artwork-afac/it25exqkctg7wpdnpruc.jpg',
     'url_large',
     1,
     1,
@@ -669,7 +669,7 @@ VALUES
     '1866',
     'La suite du commentaire est: "Ca ne lui fait rien,... tant pis pour lui ! mais aux cannes ça leur fait du bien tant mieux pour elles !...." Le jeune créole porte un chapeau de feutre déformé, pas de chaussures, comme la majorité des travailleurs. Janvier est en pleine période cyclonique: est-ce le cas ici?',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690194286/artwork-afac/orvy6dwnxwjwfenpgug3.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690291996/artwork-afac/id3jfv1rlkaq3utgrjvn.jpg',
     'url_large',
     1,
     1,
@@ -704,7 +704,7 @@ VALUES
     '1866',
     'Visiblement, Mortier de Trévise a été impressionné par la chevelure de Tina. Encore une petite fille de Victorine, plus jeune. il semble que les fillettes fassent leur apprentissage de domestiques dans la propriété des Kerveguen.',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690194430/artwork-afac/t6dcwhif9xh1mjzkxh0a.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690291994/artwork-afac/erbttc7oregcweyiijvd.jpg',
     'url_large',
     1,
     1,
@@ -739,7 +739,7 @@ VALUES
     '1861',
     'Elise est une petite fille de Victorine, issue de sa fille Coralie.',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690195206/artwork-afac/zxnspqtjhkfbnebjtz2d.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690291994/artwork-afac/hdhyhyfhatspeixjdyur.jpg',
     'url_large',
     1,
     1,
@@ -774,7 +774,7 @@ VALUES
     '1866',
     'Une autre petite fille de Victorine, sans doute dans la maison des Casernes.',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690195309/artwork-afac/thtydkj36zcoh8l9mham.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690291994/artwork-afac/bqtdyen8m7dwun1hwqmg.jpg',
     'url_large',
     1,
     1,
@@ -810,7 +810,7 @@ VALUES
     '1861',
     'La suite du commentaire est: "Allons, Virasami, vivement mettre la racine de ce plant (?) comme à Madras!".',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690200618/artwork-afac/vnwqiqooukn4cdcfrk8g.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690292000/artwork-afac/soc3w3wtdkhkxvhyehhy.jpg',
     'url_large',
     1,
     1,
@@ -845,7 +845,7 @@ VALUES
     '1871',
     'Boutchiana est devenu le domestique personnel de Ch.Mortier de Trévise, et il a vieilli de 6 ans.',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690201122/artwork-afac/rxqximl3g9julw6am3je.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690291995/artwork-afac/dkfp79subcvmsrjax2jj.jpg',
     'url_large',
     1,
     1,
@@ -880,7 +880,7 @@ VALUES
     '1861',
     'Titre complet: "Chanvert descend le chemin de la Plaine, Golo est à ses côtés". Chanvert est peut-etre un ami de la famille. Golo est un domestique qui l''accompagne. A l''arrière du tilbury, il semble qu''il y ait une borne kilométrique sur le côté de la route. Le chemin de la Plaine relie Saint-Pierre à la Plaine des Cafres, et, au-delà, à Saint-Benoît. L''Etablissement de Bel-Air est situé au tiers du parcours, entre La Plaine des Cafres et Saint-Pierre.',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690202561/artwork-afac/uozfc5y8uu3yko8hoyol.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690291993/artwork-afac/t7gb2l91s4nyqtkguhpf.jpg',
     'url_large',
     1,
     1,
@@ -915,7 +915,7 @@ VALUES
     '1866',
     'Titre complet: "Arrivée à l''établissement du Tampon". Le chemin de l''Etablissement existe toujours aujourd''hui, à 400 mètres d''altitude. Les deux cavaliers sont sans doute Ch. H. N; Mortier de Trévise lui-même, et son beau-frère (Denis-André de K/véguen)? En avant, 3 autres personnages cheminent à pied. La route traverse le lit desseché de la Rivière d''Abord, et remonte légèrement vers l''Etablissement (c''est-à-dire l''ensemble du fonds avec usine, bâtiments annexes, et camp des travailleurs engagés, non représenté ici. L''usine elle-même est composée de deux corps parallèles de bâtiments, flanqués chacun d''une cheminée: l''une pour évacuer les fumées de combustion pour la batterie Gimart, l''autre la fumée de la machine à vapeur. En quinconce, un autre bâtiment à l''avant, abritant les "tables" pour le séchage du sucre?',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690202041/artwork-afac/pihnfwvfdupqqwtojtjb.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690291995/artwork-afac/lmt4y1buefjxwvcm7ael.jpg',
     'url_large',
     1,
     1,
@@ -950,7 +950,7 @@ VALUES
     '1866',
     'Titre complet: "Tampon-Une usine". Une autre vue de l''usine de Bel Air, au Tampon: on retrouve le bâtiment en quinconce accolé au corps de l''usine, avec ses deux cheminées. Au premier plan, sur le chemin de l''Etablissement (400 m. d''altitude), on distingue un groupe de travailleurs engagés, près d''un point d''eau: un homme, une femme avec un bébé qui porte une jarre sur la tête, et un autre personnage. L''auteur note le nom des arbres et plantes (aloés divers, vacoas, palmiers)',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690201503/artwork-afac/urezyrk8cczmtoametok.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690291997/artwork-afac/kltvlkf6y2nx5jqlkew7.jpg',
     'url_large',
     1,
     1,
@@ -985,7 +985,7 @@ VALUES
     '1866',
     'Titre complet: "Effet de nuit sur la Cheminée usine du Tampon". Attribuée parfois à l''usine du Grand Tampon, mais c''est peu probable: l''usine du Grand Tampon ayant été une scierie. Or, ici, il s''agit sans doute de l''usine de Bel Air: on reconnaît les deux corps principaux du bâtiment industriel (purgerie et bâtiment abritant la machine à vapeur) en parallèle, comme sur les figures 2 et 3. La cheminée carrée est sur le côté Nord, construite en basalte, avec intercalation de poutres deux côtés par deux côtés. Devant, un gardien, dont l''ombre se projette sur la cheminée. En arrière-plan, une allée de palmiers, qui semble mener vers la maison de maître. La disposition des lieux correspond à celle qui existait à Bel Air. Scène d''apparence paisible ?',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690202217/artwork-afac/oe99la7khv9rrg7iv7ur.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690291995/artwork-afac/sah9bw9izzo6ybtktcjd.jpg',
     'url_large',
     1,
     1,
@@ -1020,7 +1020,7 @@ VALUES
     '1866',
     'Titre complet: "Quartier St Pierre. Etablissement de la Rivière, montagnes de l''Entre Deux". L''usine (Etablissement) est installée rive gauche de la Rivière Saint-Etienne, au débouché du lieu-dit l''Entre-Deux. Elle semble présenter la même physionomie que les autres établissements achetés ou construits par Gabriel de K/Véguen: 2 corps principaux de bâtiments, ici décalés l''un par rapport à l''autre, avec des ouvertures en arc de cercle pou évacuer la chaleur, la cheminée qui évacue les fumées de la batterie Gimart, et, à l''arrière, un ou deux bâtiments pour le séchage du sucre. Au Premier plan, une escouade (une "bande") de travailleurs engagés effectue la "trouaison", pour la replantation de cannes à sucre, sous la direction d''un Commandeur, vêtu d''un pantalon de toile bleue. Un vacoa est ici le témoin indispensable de l''usage de ses feuilles pour le tressage de sacs, destinés ensuite à transporter le sucre produit. ',
     'url_small',
-    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690202313/artwork-afac/t3v72oqkupnbvp2gmvmb.jpg',
+    'https://res.cloudinary.com/didpqnqdv/image/upload/v1690291993/artwork-afac/n92rofy6o6ha662fzuaq.jpg',
     'url_large',
     1,
     1,
@@ -1036,14 +1036,9 @@ VALUES
 INSERT INTO
   entity (name)
 VALUES
-  ('Company A'),
-  ('Company B');
-
-INSERT INTO
-  entity (name)
-VALUES
-  ('Organization X'),
-  ('Organization Y');
+  ('AFAC-974'),
+  ('École'),
+  ('Université');
 
 -- Table user
 INSERT INTO
@@ -1162,6 +1157,30 @@ VALUES
     1
   );
 
+  
+INSERT INTO
+  user (
+    lastname,
+    firstname,
+    pseudo,
+    email,
+    image,
+    hashedPassword,
+    role,
+    entity_id
+  )
+VALUES
+  (
+    'Wick',
+    'John',
+    'John Wick',
+    'johnwick@exemple.com',
+    'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    '$argon2id$v=19$m=65536,t=5,p=1$xqg/EtAgnK8Bg07oBXBQEQ$He+fQghs6IVFwzHd4GhUGBgEJU78BcGRN6lPXc0wT00',
+    1,
+    1
+  );
+
 INSERT INTO
   user (
     lastname,
@@ -1189,7 +1208,12 @@ VALUES
 INSERT INTO
   artwork_favorite (user_id, artwork_id)
 VALUES
-  (1, 1);
+  (6, 1),
+  (6, 5),
+  (6, 12),
+  (6, 21),
+  (6, 8),
+  (6, 6);
 
 INSERT INTO
   artwork_favorite (user_id, artwork_id)
@@ -1200,7 +1224,8 @@ VALUES
 INSERT INTO
   artist_technique (artist_id, technique_id)
 VALUES
-  (1, 1);
+  (1, 1),
+  (1, 2);
 
 -- Table art_trend_artist
 INSERT INTO
